9c82f9a3d1939628ca1bad0d3b86195093340996
melchized3k <64768668+melchized3k@users.noreply.github.com>
2020-05-12 17:51:17 +0800

    Update index.php
    Add array variables

--- index.php
+++ index.php
@@ -6,18 +6,17 @@
   <body>
     <?php
       $name = "PHP Store";
-      $credit = 1000;
+      $credit = 1000; 
 
       echo "<h1>Welcome to ".$name."!</h1>";
-      echo "<h2> You have $".$credit." in your wallet.</h2>"
-            $products['COmputer']=750;
-            $products['Car']=15000;
-            $products[iPhone]=1000;
-            $products['Toaster']=75;
+      echo "<h2>You have $".$credit." in your wallet.</h2>";
 
-            echo "<p> A car costs $'.$products['Car']."</p>";
-            
+	    $products['Computer']=750;
+	    $products['Car']=15000;
+	    $products['iPhone']=1000;
+	    $products['Toaster']=75;
 
+	    echo "<p>A car costs $".$products['Car']."</p>";
     ?>
   </body>
 </html>
