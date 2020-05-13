ad79edca5466c7aac8b17573acc9cd46aa89be2b
melchized3k <64768668+melchized3k@users.noreply.github.com>
2020-05-14 01:37:45 +0800

    git add index.php git commit -m"add tax calc function" git push origin master

--- index.php
+++ index.php
@@ -27,6 +27,13 @@
                 echo "<p>".$key."</p>";
               }
             }
+            echo tax_calc(750,0.223);
+            function tax_calc($amount,$tax){
+              $calculate_tax = $amount*$tax;
+              $amount = round($amount+$calculate_tax,2);
+              return $amount;
+            
+            }
     ?>
   </body>
 </html>
